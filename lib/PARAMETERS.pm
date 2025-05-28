92f851c99f3585ec144de7719f9921db9f4363b2
regiov <renato@cria.org.br>
2025-05-28 16:58:31 -0300

    Decodificação seletiva no local correto

--- lib/PARAMETERS.pm
+++ lib/PARAMETERS.pm
@@ -26,23 +26,6 @@ sub new
 #print OUT $buf;
 #close OUT;
 
-    # Tratamento para UTF8
-    foreach my $key (keys %{$self->{'data'}}) {
-      foreach my $i (0..$#{$self->{'data'}->{$key}}) {
-        # Não decodifica se houver um campo filename correspondente (ou seja, se for um arquivo)
-        next if defined $self->{'filename'}->{$key}[$i];
-
-        my $val = $self->{'data'}->{$key}[$i];
-
-        # Decodifica string como UTF-8
-        if (!Encode::is_utf8($val)) {
-          $val = Encode::decode('utf-8', $val);
-        }
-
-        $self->{'data'}->{$key}[$i] = $val;
-      }
-    }
-
     if ($encode)
     { my $buf1 = Encode::decode('utf-8',$buf,Encode::FB_HTMLCREF);
       $buf = $buf1 if $buf1;
@@ -134,6 +117,23 @@ sub new
             }
             $self->{'filename'}->{$name}[$k] =~ s/_PERCENT_CHAR_/%/g;
           }
+        }
+      }
+
+      # Tratamento para UTF8
+      foreach my $key (keys %{$self->{'data'}}) {
+        foreach my $i (0..$#{$self->{'data'}->{$key}}) {
+          # Não decodifica se houver um campo filename correspondente (ou seja, se for um arquivo)
+          next if defined $self->{'filename'}->{$key}[$i];
+
+          my $val = $self->{'data'}->{$key}[$i];
+
+          # Decodifica string como UTF-8
+          if (!Encode::is_utf8($val)) {
+            $val = Encode::decode('utf-8', $val);
+          }
+
+          $self->{'data'}->{$key}[$i] = $val;
         }
       }
     }
